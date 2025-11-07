e07a3057fb12a7179758e6b31a0067c61157a016
Perpetual Echelon <diamondbackspartan@gmail.com>
2025-11-07 11:24:18 -0500

    Add LunaSettings check to SolPlugin

--- src/bugatti/plugins/SolPlugin.java
+++ src/bugatti/plugins/SolPlugin.java
@@ -3,12 +3,18 @@ package bugatti.plugins;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.BaseModPlugin;
 import bugatti.scripts.SolMegaSpawner;
+import lunalib.lunaSettings.LunaSettings;
 
 public class SolPlugin extends BaseModPlugin {
 
     @Override
     public void onNewGameAfterEconomyLoad() {
-        Global.getLogger(this.getClass()).info("[SOL] Spawning Megastructures on Sol...");
-        Global.getSector().addScript(new SolMegaSpawner());
+        Boolean setting = LunaSettings.getBoolean("sol-modded", "featuresDisabled");
+        if (setting == null || setting) { // default = true if missing
+            Global.getLogger(this.getClass()).info("[SOL] Reticulating Splines on Sol...");
+            Global.getSector().addScript(new SolMegaSpawner());
+        } else {
+            Global.getLogger(this.getClass()).info("[SOL] MegaSpawner disabled in LunaSettings.");
+        }
     }
 }
